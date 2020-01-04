89080d5573c0113f4148c283769b82fcf3e8996e
J W <root@home.local>
2020-01-04 20:00:58 +0000

    Added comma to fix typo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='weather-getter',
       version='0.1',
-      description='Similar weather getter api from openweathermap; adds to influxdb'
+      description='Similar weather getter api from openweathermap; adds to influxdb',
       url='http://github.com/',
       author='jw',
       author_email='jw@a.b',
